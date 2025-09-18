7032d5376697556dfdbdc9af8346ff8817d949fa
asmauued <frostmada256@gmail.com>
2025-09-18 13:27:32 +0700

    update

--- file/sw.js
+++ file/sw.js
@@ -1,17 +1,15 @@
-
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('v1').then(cache => {
       return cache.addAll([
-        'SUKASEHAT/',
-        'SUKASEHAT/index.html',
-        'SUKASEHAT/error.html',
-        'SUKASEHAT/script.js'
+        '/',   
+        '/index.html',
+        '/error.html',
+        '/script.js'
       ]);
     })
   );
 });
-
 self.addEventListener('fetch', event => {
   event.respondWith(
     fetch(event.request).catch(() => caches.match('/error.html'))
